47b8423f0ef1389ea4611dc156c7a53fb60e1f59
ThullioF <thullioferreira@hotmail.com>
2024-03-11 17:51:24 -0300

    first

--- Main.py
+++ Main.py
@@ -4,3 +4,11 @@ class Main:
 print("Testando o projeto")
 
 from Cliente import Cliente
+
+from Conta import Conta
+
+c1 = Cliente("João","163706-6584")
+
+conta = Conta(c1.nome,6565,0)
+
+print(conta.titular, " Núm ero: ",conta.numero, " Seu saldo: ",conta.saldo)
